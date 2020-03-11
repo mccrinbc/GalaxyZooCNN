d55c8c18e2f301eada7ad963211e8756e7428b7c
Brian McCrindle <brianmccrindle@Brians-MacBook-Pro.local>
2020-03-11 12:02:18 -0400

    Organized for test/train files on Dataloader

--- moveFiles.py
+++ moveFiles.py
@@ -3,24 +3,34 @@ import torch
 
 PATH = "/Users/brianmccrindle/Documents/736/galaxy-zoo-the-galaxy-challenge"
 IDs = torch.load(PATH + '/IDs.pt')
-classLabels = torch.load(PATH + '/classLabels.pt')
+classLabels = torch.load(PATH + '/classLabels.pt') #these are randomally sorted already
+
+trainSize = round(len(classLabels)*0.70) #70% of data for train
+
+IDTrain = IDs[0:trainSize] 
+IDTest  = IDs[trainSize+1 : len(classLabels)]
+classLabelsTrain = classLabels[0:trainSize] 
+classLabelsTest  = classLabels[trainSize+1 : len(classLabels)]
 
 curImgPath = PATH + '/preprocessed/'
 
-#count the number of images in each class
-C1 = 0
-C2 = 0
-C3 = 0
-
-for ii in range(len(IDs)):
-	name = str(IDs[ii].item()) + '.jpg'
-	if classLabels[ii][0] == 1:
-		shutil.move(curImgPath + name, curImgPath + 'C1/' + name)
-		C1 += 1
-	elif classLabels[ii][1] == 1:
-		shutil.move(curImgPath + name, curImgPath + 'C2/' + name)
-		C2 += 1
+#for training organizatiopn
+for ii in range(len(IDTrain)):
+	name = str(IDTrain[ii].item()) + '.jpg'
+	if classLabelsTrain[ii][0] == 1:
+		shutil.move(curImgPath + 'C1/' + name, curImgPath + 'train/C1/' + name)
+	elif classLabelsTrain[ii][1] == 1:
+		shutil.move(curImgPath + 'C2/' + name, curImgPath + 'train/C2/' + name)
 	else:
-		shutil.move(curImgPath + name, curImgPath + 'C3/' + name)
-		C3 += 1
+		shutil.move(curImgPath + 'C3/' + name, curImgPath + 'train/C3/' + name)
+
+#for testing organizatiopn
+for ii in range(len(IDTest)):
+	name = str(IDTest[ii].item()) + '.jpg'
+	if classLabelsTest[ii][0] == 1:
+		shutil.move(curImgPath + 'C1/' + name, curImgPath + 'test/C1/' + name)
+	elif classLabelsTest[ii][1] == 1:
+		shutil.move(curImgPath + 'C2/' + name, curImgPath + 'test/C2/' + name)
+	else:
+		shutil.move(curImgPath + 'C3/' + name, curImgPath + 'test/C3/' + name)
 	
