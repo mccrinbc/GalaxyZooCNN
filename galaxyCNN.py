68588277fd12509e8a4db1b95219938cca8c94c0
Brian McCrindle <brianmccrindle@Brians-MacBook-Pro.local>
2020-03-14 12:34:18 -0400

    Classification CNN Code

---
+++ galaxyCNN.py
@@ -0,0 +1,157 @@
+import os 
+from tqdm import tqdm #loading bar 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import transforms
+
+import torch.optim as optim
+
+dir_path = '/Users/brianmccrindle/Documents/736/galaxy-zoo-the-galaxy-challenge'
+os.chdir(dir_path) #change to this working directory 
+
+#imgIDs = torch.load(dir_path +'/IDs.pt')
+
+#class to call the data from local machine
+class buildImgStack():
+	classLocs = os.getcwd() + '/preprocessed/'
+	imgIDs = torch.load(os.getcwd() + '/IDs.pt')
+	classLabels = torch.load(os.getcwd() + '/classLabels.pt')
+
+	transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), 
+                                    transforms.ToTensor()])
+
+	def load_dataset(self, type):
+	    if type == 'train':
+	    	data_path = self.classLocs + 'train/'
+	    	shuffle_status = True
+	    elif type == 'test':
+	    	data_path = self.classLocs + 'test/'
+	    	shuffle_status = False
+
+	    dataset = torchvision.datasets.ImageFolder(
+	        root = data_path,
+	        transform = self.transform
+	    )
+	    dataLoader = DataLoader(
+	        dataset,
+	        batch_size = 64,
+	        num_workers = 0, #parallel processing
+	        shuffle = shuffle_status
+	    )
+	    #classes are sorted in alphabetical order from folders when using dataLoader
+
+	    #classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+		#classes.sort()
+	    return dataLoader
+
+
+
+# Building a simple multi-layer CNN for image classification 
+class convNet(nn.Module):
+	def __init__(self):
+		super().__init__()
+					#nn.Conv2d(input_channels, output_channels, kernal_size)
+		self.conv1 = nn.Conv2d(1,  32,  3, stride = 1, padding = 0, bias = True) 
+		self.conv2 = nn.Conv2d(32, 64,  3, stride = 1, padding = 0, bias = True) 
+		self.conv3 = nn.Conv2d(64, 128, 3, stride = 1, padding = 0, bias = True) 
+
+		#this is to get the size of the input to FCL
+		x = torch.randn([191,191]).view(-1,1,191,191)
+		self.input = None
+		self.convolve(x)
+
+		#the size of the input (output from conv3 should be 185)
+		self.fc1   = nn.Linear(self.input,128) 
+		self.fc2   = nn.Linear(128,3) #three output classes (Smooth, Disk, Artifact)
+
+	def convolve(self,x):
+		x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)) #pooling layers 2x2
+		x = F.max_pool2d(F.relu(self.conv2(x)),(2,2))
+		x = F.max_pool2d(F.relu(self.conv3(x)),(2,2))
+
+		if self.input is None:
+			#input shape to the fully connected layer
+			self.input = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+			print(self.input)
+		return x
+
+	def forward(self,x):
+		x = self.convolve(x)
+		x = x.view(-1, self.input) #flatten the image for the fully-connected layers
+
+		x = F.relu(self.fc1(x))
+		x = self.fc2(x)
+
+		#return F.softmax(x, dim = 1) #the x in this case could be a batch of x's
+		return x #only need x if CrossEntropyLoss
+
+
+model = convNet()
+images = buildImgStack()
+dataLoaderTrain = images.load_dataset(type = 'train')
+dataLoaderTest =  images.load_dataset(type = 'test' )
+
+#activate the GPU if avaliable 
+if torch.cuda.is_available():
+	device = torch.device("cuda:0")
+	print("Running on GPU")
+else:
+	device = torch.device("cpu")
+	print("Running on CPU")
+
+model = convNet().to(device)
+print(model)
+
+def trainModel(model):
+	#we need to place the optimizer and criterion in the function if we
+	#want to run the model on the GPU. 
+	EPOCHS = 1 #for now 
+	optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum = 0.90)
+	criterion = nn.CrossEntropyLoss() #this might not work?
+	for epoch in range(EPOCHS):
+		for batch_idx, (data, target) in enumerate(dataLoaderTrain):
+			print(batch_idx)
+			#need to put these on the GPU if avaliable 
+			#print(data.to(device))
+			#print(target.to(device))
+
+			#This is a bit of a hack but whatever
+			labels = []
+			for j in range(0,len(target)):
+				labels.append(np.eye(3)[target[j].item()])
+
+			labels = torch.Tensor(labels) #need to convert the labels into a tensor
+
+			model.zero_grad()
+			output = model(data)
+			#return output, data, labels
+			loss = criterion(output,target)
+			loss.backward()
+			optimizer.step()
+			print(f"Epoch Loss: {loss}")
+	print(f"Epoch: {epoch}. Loss: {loss}")
+
+#output,data,labels = trainModel(model)
+
+def testModel(model):
+	correct = 0
+	total = 0
+	with torch.no_grad():
+		for batch_idx, (data, target) in enumerate(dataLoaderTest):
+			return data,target
+
+
+data,target = testModel(model)
+
+
+
+
+
+
+
